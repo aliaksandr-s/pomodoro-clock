7be63b0be1f33e0e5db9d821a04d26a8c7e5cb21
aliaksandr-s <s-a-n-9@mail.ru>
2017-06-26 22:33:28 +0300

    Update Clock tests

--- src/containers/Clock/Clock.test.js
+++ src/containers/Clock/Clock.test.js
@@ -6,3 +6,12 @@ it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<Clock />, div)
 })
+
+test('time should be correct', () => {
+  const clock = new Clock()
+  expect(clock.state.workLength.format('mm:ss')).toBe('25:00')
+  expect(clock.state.workLength.add(10, 'm').format('mm:ss')).toBe('35:00')
+  expect(clock.state.workLength.subtract(5, 'm').format('mm:ss')).toBe('30:00')
+  expect(clock.state.breakLength.add(5, 'm').format('mm:ss')).toBe('10:00')
+  expect(clock.state.breakLength.subtract(10, 'm').format('mm:ss')).toBe('00:00')
+})
