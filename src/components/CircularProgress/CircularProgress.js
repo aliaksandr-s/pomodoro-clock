84c22ec4e89c0818a3bb15e913c91bbf65a96fd7
Your Name <you@example.commmm>
2017-07-02 20:48:25 +0300

    Add CircularProgress component

---
+++ src/components/CircularProgress/CircularProgress.js
@@ -0,0 +1,19 @@
+// @flow
+import React from 'react'
+import CircularProgressbar from 'react-circular-progressbar'
+import './CircularProgress.css'
+
+type Props = {
+  percentage: number,
+  classForPercentage: () => void
+}
+
+const CircularProgress = (props: Props): React.Element<*> => (
+  <CircularProgressbar
+    percentage={props.percentage}
+    strokeWidth={2}
+    classForPercentage={props.classForPercentage}
+  />
+)
+
+export default CircularProgress
