dc9e77db24d97c0614ac9b4be9607cca65b4b769
Your Name <you@example.commmm>
2017-07-03 22:47:48 +0300

    Update timer

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,15 +1,19 @@
 // @flow
 
 import React from 'react'
+import './Timer.css'
 
 type Props = {
-  value: string,
+  minutes: string,
+  seconds: string,
   className: string
 }
 
 const Timer = (props: Props): React.Element<*> => (
   <div className={props.className} >
-    {props.value}
+    <div className="timer__time">{props.minutes}</div>
+    <span>:</span>
+    <div className="timer__time">{props.seconds}</div>
   </div>
 )
 
