cf5ef0967a3ddf81c3e448f6fb4021e23f657d04
Your Name <you@example.commmm>
2017-07-09 18:18:32 +0300

    Update tests

--- src/reducers/index.test.js
+++ src/reducers/index.test.js
@@ -1,4 +1,4 @@
-import { INCREASE_COUNTER, DECREASE_COUNTER } from 'actions/index'
+import { INCREASE_COUNTER, DECREASE_COUNTER, START, STOP } from 'actions/index'
 import { COUNTER_TYPES } from 'containers/Counters/Counters'
 import moment from 'moment'
 import reducer from './index'
@@ -7,7 +7,25 @@ const INITIAL_STATE = {
   time: moment(),
   workLength: moment().minute(25).second(0),
   breakLength: moment().minute(5).second(0),
+  clock: moment().minute(25).second(0),
+  isStopped: true,
 }
+
+test('Start stop', () => {
+  expect(
+    reducer(INITIAL_STATE, {
+      type: START,
+      payload: null,
+    }).isStopped)
+  .toBe(false)
+
+  expect(
+    reducer(INITIAL_STATE, {
+      type: STOP,
+      payload: null,
+    }).isStopped)
+  .toBe(true)
+})
 
 test('Counter Work', () => {
   expect(
@@ -23,6 +41,20 @@ test('Counter Work', () => {
       payload: COUNTER_TYPES.work,
     }).workLength.format('mm:ss'))
   .toBe('24:00')
+
+  expect(
+    reducer({ ...INITIAL_STATE, clock: moment().minute(24).second(20) }, {
+      type: INCREASE_COUNTER,
+      payload: COUNTER_TYPES.work,
+    }).workLength.format('mm:ss'))
+  .toBe('25:00')
+
+  expect(
+    reducer({ ...INITIAL_STATE, clock: moment().minute(24).second(20) }, {
+      type: DECREASE_COUNTER,
+      payload: COUNTER_TYPES.work,
+    }).workLength.format('mm:ss'))
+  .toBe('25:00')
 })
 
 test('Counter Break', () => {
@@ -39,4 +71,18 @@ test('Counter Break', () => {
       payload: COUNTER_TYPES.break,
     }).breakLength.format('mm:ss'))
   .toBe('04:00')
+
+  expect(
+    reducer({ ...INITIAL_STATE, clock: moment().minute(24).second(20) }, {
+      type: INCREASE_COUNTER,
+      payload: COUNTER_TYPES.break,
+    }).breakLength.format('mm:ss'))
+  .toBe('05:00')
+
+  expect(
+    reducer({ ...INITIAL_STATE, clock: moment().minute(24).second(20) }, {
+      type: DECREASE_COUNTER,
+      payload: COUNTER_TYPES.break,
+    }).breakLength.format('mm:ss'))
+  .toBe('05:00')
 })
